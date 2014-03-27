910ae0cd931f0cc3ca2805a2f4cdc46e956fee24
Tom Marshall <tdm@cyngn.com>
2014-03-27 09:18:00 -0700

    sr: Touch UI
    Change-Id: I1c9b78c8fba3c23f2ecddb7659746af0bd12ee55

--- Android.mk
+++ Android.mk
@@ -107,6 +107,10 @@ ifneq ($(BOARD_RECOVERY_BLDRMSG_OFFSET),)
     LOCAL_CFLAGS += -DBOARD_RECOVERY_BLDRMSG_OFFSET=$(BOARD_RECOVERY_BLDRMSG_OFFSET)
 endif
 
+ifneq ($(BOARD_RECOVERY_NEEDS_REL_INPUT),)
+    LOCAL_CFLAGS += -DBOARD_RECOVERY_NEEDS_REL_INPUT
+endif
+
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
 # It gets copied there in config/Makefile.  LOCAL_MODULE_TAGS suppresses
 # a (redundant) copy of the binary in /system/bin for user builds.
