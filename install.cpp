f456cd8e5b5b82eb984cd9f924dc58971036a2ac
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-10 12:01:33 +0100

    install: Ditch the generic error
    Try to replace it with something more meaningful
    
    Change-Id: Ifc59ccdc1cf2a71245428ab9ff6495dc6c51d8b3

--- install.cpp
+++ install.cpp
@@ -170,7 +170,11 @@ try_update_binary(const char *path, ZipArchive *zip, int* wipe_cache) {
     int status;
     waitpid(pid, &status, 0);
     if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-        LOGE("Error in %s\n(Status %d)\n", path, WEXITSTATUS(status));
+        if (status != 7) {
+           LOGE("Installation error in %s\n(Status %d)\n", path, WEXITSTATUS(status));
+        } else {
+           LOGE("Failed to install %s\nTake note of all the above lines for reports\n", path, WEXITSTATUS(status));
+        }
         return INSTALL_ERROR;
     }
 
