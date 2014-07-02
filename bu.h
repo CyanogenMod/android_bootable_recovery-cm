fb4ba6cfc05bd262dbecb9c29462068f8a2f6a72
Tom Marshall <tdm@cyngn.com>
2014-07-02 08:55:57 -0700

    sr: bu: Show backup/restore progress in dialog text
    Change-Id: Id8d7fde4d9bd8b300dc1ee5ea55ac17651019c0f

--- bu.h
+++ bu.h
@@ -19,6 +19,13 @@ typedef struct md5 MD5_CTX;
 #define HASH_MAX_LENGTH SHA1_DIGEST_LENGTH
 #define HASH_MAX_STRING_LENGTH SHA1_DIGEST_STRING_LENGTH
 
+#ifndef min
+#define min(a,b) ((a)<(b)?(a):(b))
+#endif
+#ifndef max
+#define max(a,b) ((a)<(b)?(b):(a))
+#endif
+
 #define PROP_LINE_LEN (PROPERTY_KEY_MAX+1+PROPERTY_VALUE_MAX+1+1)
 
 #define PATHNAME_SOD "/tmp/sod"
@@ -33,9 +40,26 @@ extern size_t hash_datalen;
 extern SHA1_CTX sha1_ctx;
 extern MD5_CTX md5_ctx;
 
+struct partspec {
+    char*       name;
+    char*       path;
+    fstab_rec*  vol;
+    uint64_t    size;
+    uint64_t    used;
+    uint64_t    off;
+};
+#define MAX_PART 8
+
 extern void logmsg(const char* fmt, ...);
+
+extern int part_add(const char* name);
+extern partspec* part_get(int i);
+extern partspec* part_find(const char* name);
+extern void part_set(partspec* part);
+
+extern int update_progress(uint64_t off);
+
 extern int create_tar(const char* compress, const char* mode);
 
 extern int do_backup(int argc, char** argv);
 extern int do_restore(int argc, char** argv);
-
