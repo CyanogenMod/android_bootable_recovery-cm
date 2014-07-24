0217c8fa6589c8f38bd4d567d8217527e4fe4d10
philz-cwm6 <phytowardt@gmail.com>
2014-07-25 00:13:27 +0200

    SR: Fix datamedia detection
    Devices with secondary storage but no primary storage should be
    detected as datamedia.
    
    Rules for determining primary storage are:
    
     * Static mounts named /sdcard are primary storage except when the
       type is declared as datamedia.
    
     * Static mount points beginning with /mnt/media_rw/sdcard are
       primary storage unless followed by a non-zero value.
    
     * Dynamic mount points beginning with sdcard are primary storage
       unless followed by a non-zero value.
    
    Change-Id: Ica6e967f98e4506d85095a58b6ede774db64956d

--- roots.cpp
+++ roots.cpp
@@ -83,6 +83,36 @@ int is_data_media()
     return is_datamedia;
 }
 
+static int is_volume_primary_storage(fstab_rec* v)
+{
+    // Static mount point /sdcard is primary storage, except when it's
+    // declared as datamedia
+    if (strcmp(v->mount_point, "/sdcard") == 0) {
+        if (strcmp(v->fs_type, "datamedia") == 0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    // Detect static and dynamic mount points named sdcard*
+    const char* endp = NULL;
+    if (strncmp(v->mount_point, "/mnt/media_rw/sdcard", 20) == 0) {
+        endp = &v->mount_point[20];
+    }
+    else if (fs_mgr_is_voldmanaged(v) && strncmp(v->label, "sdcard", 6) == 0) {
+        endp = &v->label[6];
+    }
+    if (endp) {
+        // If the name ends with a non-zero value, it's not primary
+        if (isdigit(*endp) && atoi(endp) != 0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    return 0;
+}
+
 void load_volume_table()
 {
     int i;
@@ -120,10 +150,7 @@ void load_volume_table()
 
         write_fstab_entry(v, file);
 
-        if (strcmp(v->mount_point, "/external_sd") == 0 ||
-                strncmp(v->mount_point, "/sdcard", 7) == 0 ||
-                strncmp(v->mount_point, "/mnt/media_rw/sdcard", 20) == 0 ||
-                (fs_mgr_is_voldmanaged(v) && strncmp(v->label, "sdcard", 6) == 0)) {
+        if (is_volume_primary_storage(v)) {
             is_datamedia = 0;
         }
     }
@@ -146,6 +173,11 @@ storage_item* get_storage_items() {
     }
     for (i = 0; i < get_num_volumes(); i++) {
         fstab_rec* v = get_device_volumes() + i;
+
+        // Internal storage was allocated above
+        if (strcmp(v->fs_type, "datamedia") == 0)
+            continue;
+
         if (strcmp(v->mount_point, "/external_sd") == 0 ||
                 strncmp(v->mount_point, "/sdcard", 7) == 0) {
             item->vol = v;
